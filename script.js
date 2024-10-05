8cc16cd7885d09d90184534876a4243dc36e9ec6
IainaraCerqueira <cerqueiranara10@gmail.com>
2024-10-05 16:17:33 -0300

    atualizações

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Menu Toggle para dispositivos móveis
+
 const menuToggle = document.getElementById('mobile-menu');
 const navLinks = document.querySelector('.nav-links');
 
@@ -6,7 +6,7 @@ menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-// Formulário de Contato
+
 const contactForm = document.getElementById('contact-form');
 const formMessage = document.getElementById('form-message');
 
@@ -21,36 +21,36 @@ contactForm.addEventListener('submit', function(e) {
     }, 2000);
 });
 
-// Alternar tema claro/escuro
+
 const themeToggleButton = document.getElementById('theme-toggle');
 
-// Alterna classes de tema
+
 themeToggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     document.body.classList.toggle('light-mode');
 
-    // Muda o texto do botão com base no tema atual
+    
     if (document.body.classList.contains('dark-mode')) {
-        themeToggleButton.textContent = 'Tema Claro'; // Exibe "Tema Claro" no modo escuro
+        themeToggleButton.textContent = 'Tema Claro'; 
     } else {
-        themeToggleButton.textContent = 'Tema Escuro'; // Exibe "Tema Escuro" no modo claro
+        themeToggleButton.textContent = 'Tema Escuro'; 
     }
     
-    // Armazenar a preferência do tema
+   
     localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode');
 });
 
-// Inicialização padrão do tema
+
 if (!localStorage.getItem('theme')) {
     document.body.classList.add('light-mode');
-    themeToggleButton.textContent = 'Tema Escuro'; // Inicia com "Tema Escuro"
+    themeToggleButton.textContent = 'Tema Escuro'; 
 } else {
     document.body.classList.add(localStorage.getItem('theme'));
     
-    // Atualiza o texto do botão com base no tema armazenado
+    
     if (localStorage.getItem('theme') === 'dark-mode') {
-        themeToggleButton.textContent = 'Tema Claro'; // Se o tema for escuro, exibe "Tema Claro"
+        themeToggleButton.textContent = 'Tema Claro';
     } else {
-        themeToggleButton.textContent = 'Tema Escuro'; // Se o tema for claro, exibe "Tema Escuro"
-    }
+        themeToggleButton.textContent = 'Tema Escuro';}
+
 }
